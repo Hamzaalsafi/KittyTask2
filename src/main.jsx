2c24f30ac19949ded811759d22ed0b10b3ce5458
Hamzaalsafi <hamzasf345@gmail.com>
2025-05-21 01:06:21 +0300

    run the coil case

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { MenuProvider } from './MenuProvider'; 
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { MenuProvider } from "./MenuProvider";
+import App from "./App.jsx";
 
-import './index.css';
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MenuProvider>
       <App />
