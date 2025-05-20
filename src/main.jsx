37e714e931408f4eadf3f082c4291511cdd61db4
Hamzaalsafi <hamzasf345@gmail.com>
2025-05-21 01:14:50 +0300

    run the coil case

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MenuProvider } from "./MenuProvider";
+import { MenuProvider } from "./MenuProvider.jsx";
 import App from "./App.jsx";
 
 import "./index.css";
