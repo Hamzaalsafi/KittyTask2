844b18b63f043ae2a2da870f4004cdee256fd959
Hamzaalsafi <hamzasf345@gmail.com>
2025-05-21 00:39:58 +0300

    run the coil case

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import './app.css';
-import { Bord } from './Bord';
-import { Home } from './Home';
-import { NavBar } from './NavBar';
-import { Login } from './Login';
-import {CardsArchive} from './CardsArchive';
-import { CreateBoardForNav } from'./CreateBoardForNav';
+import "react";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+import { Bord } from "./Bord";
+import { Home } from "./Home";
+import { NavBar } from "./NavBar";
+import { Login } from "./Login";
+import { CardsArchive } from "./CardsArchive";
+import { CreateBoardForNav } from "./CreateBoardForNav";
 function App() {
   return (
     <Router>
@@ -17,14 +22,14 @@ function App() {
 
 function Main() {
   const location = useLocation();
-  const Loginctl = '/';
+  const Loginctl = "/";
 
   return (
     <div className="app">
       {location.pathname !== Loginctl && <NavBar />}
       <Routes>
-        <Route path="/CardsArchive" element={<CardsArchive/>} />
-        <Route path="/Home" element={<Home/>} />
+        <Route path="/CardsArchive" element={<CardsArchive />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/Board/:id" element={<Bord />} />
         <Route path="/CreateBoard" element={<CreateBoardForNav />} />
